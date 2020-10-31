b3cb1d3b6e4d92f897d419289f48aa3c613c24b2
paulo4fs <engpaulofrota@gmail.com>
2020-10-30 22:08:34 -0300

    criando funcionalidade do botao

--- app/src/main/java/com/example/aula28todo/listatarefas/viewmodel/TarefaViewModel.kt
+++ app/src/main/java/com/example/aula28todo/listatarefas/viewmodel/TarefaViewModel.kt
@@ -1,10 +1,29 @@
 package com.example.aula28todo.listatarefas.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.aula28todo.listatarefas.model.Tarefa
 import com.example.aula28todo.listatarefas.repository.TarefaRepository
 
 class TarefaViewModel(
     private val repository: TarefaRepository
 ) : ViewModel() {
+    val tarefas = MutableLiveData<List<Tarefa>>()
 
+    fun obterLista() {
+        repository.obterLista { lista ->
+            tarefas.value = lista
+        }
+        repository.criarItem()
+    }
+
+    class ListaTarefaViewModelFactory(
+        private val repository: TarefaRepository
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return TarefaViewModel(repository) as T
+        }
+
+    }
 }
